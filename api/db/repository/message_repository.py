8068d36e18aa8a72d13112f56fcd35b58440bf87
pablo <konndann02@mail.ru>
2026-07-07 16:45:17 +0300

    update last_event_at on message create

--- api/db/repository/message_repository.py
+++ api/db/repository/message_repository.py
@@ -3,6 +3,7 @@ import logging
 from sqlalchemy import select
 
 from db.models.message import Message
+from db.repository.private_chat_repository import PrivateChatRepository
 from db.exceptions import DatabaseError
 
 class MessageRepository:
@@ -17,6 +18,7 @@ class MessageRepository:
             self._session.add(message)
             await self._session.commit()
             await self._session.refresh(message)
+            await PrivateChatRepository(self._session).update_last_event(chat_id)
             return message
         except Exception as e:
             self._logger.error(f"Error at user creating message {{chat_id: {chat_id}, from_user_id: {from_user_id}}}: {e}")
